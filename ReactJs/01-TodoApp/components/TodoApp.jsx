2a5c3dd003e9cbff376430996b336b166bde97fc
keshav <keshav.bgir.980@gmail.com>
2025-01-14 00:54:57 +0530

    make interactive todo app

--- ReactJs/01-TodoApp/components/TodoApp.jsx
+++ ReactJs/01-TodoApp/components/TodoApp.jsx
@@ -2,8 +2,40 @@ import { useState } from 'react';
 import style from './TodoStyle.module.css';
 
 function TodoApp(){
-  const [task, setTask] = useState([]);
+  const [tasks, setTask] = useState(["wake up", "job", "eate", "sleep"]);
+  const[newTask, setNewTask] = useState();
   
+  function handleInputChanges(e){
+    setNewTask(e.target.value)
+  }
+  function handleAddNewTask(){
+    if(newTask.trim() !== ""){
+      setTask(t=> [...t, newTask]);
+      setNewTask("");
+    }
+    else
+    {
+      alert("no task");
+    }
+  }
+  function handleRemoveTask(index){
+    const updatedTask = tasks.filter((_,i)=> i !== index);
+    setTask(updatedTask);
+  }
+  function handleMoveUp(index){
+    if(index > 0){
+      const updatedTask = [...tasks];
+      [updatedTask[index], updatedTask[index - 1]] = [updatedTask[index - 1], updatedTask[index]]
+      setTask(updatedTask);
+    }
+  }
+  function handleMoveDown(index){
+    if(index < tasks.length - 1){
+      const updatedTask = [...tasks];
+      [updatedTask[index], updatedTask[index + 1]] = [updatedTask[index + 1], updatedTask[index]];
+      setTask(updatedTask);
+    }
+  }
 
 
   return(
@@ -19,8 +51,16 @@ function TodoApp(){
         </div>
         <div>
           <div className={style.inputContainer}>
-            <input type="text" placeholder="Add Task ToDo..."  className={style.taskInput}/>
-            <button className={style.addTaskBtn}>
+            <input
+             type="text"
+             placeholder="Add Task ToDo..."
+             className={style.taskInput}
+             onChange={handleInputChanges}
+             value={newTask}
+             />
+            <button 
+            onClick={handleAddNewTask}
+            className={style.addTaskBtn}>
               {/* <img src="../public/add.svg" alt="" /> */}
               <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/></svg>
               Add
@@ -33,23 +73,27 @@ function TodoApp(){
           </div>
           <div className={style.listContainer}>
             <ul className={style.taskList}>
-                <li>
-                  <div className={style.todoNameContainer}>
-                    <span className={style.todoCount}>1.</span>
-                    <span className={style.todoName}>wake up</span>
-                  </div>
-                  <div className={style.todoBtnContainer}>
-                    <button>
-                      <img src="../public/delete.svg" alt="" />
-                    </button>
-                    <button>
-                      <img src="../public/arrow_upward.svg" alt="" />
-                    </button>
-                    <button>
-                      <img src="../public/arrow_downward.svg" alt="" />
-                    </button>
-                  </div>
-                </li>
+                {
+                  tasks.map((task, index)=>
+                    <li key={index}>
+                      <div className={style.todoNameContainer}>
+                        <span className={style.todoCount}>{index+1}.</span>
+                        <span className={style.todoName}>{task}</span>
+                      </div>
+                      <div className={style.todoBtnContainer}>
+                        <button onClick={()=>handleRemoveTask(index)}>
+                          <img src="../public/delete.svg" alt="" />
+                        </button>
+                        <button onClick={()=>handleMoveUp(index)}>
+                          <img src="../public/arrow_upward.svg" alt="" />
+                        </button>
+                        <button onClick={()=>handleMoveDown(index)}>
+                          <img src="../public/arrow_downward.svg" alt="" />
+                        </button>
+                      </div>
+                    </li>
+                  )
+                }
             </ul>
           </div>
         </div>
